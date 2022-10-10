fedcc5d2a6c104982ba8d8e024ffc3393fe3efc3
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-10-10 21:24:06 +0300

    Update дз3.py

--- дз3.py
+++ дз3.py
@@ -1,103 +1,93 @@
 "# -- coding: utf-8 --"
-x = int(input())
-y = int(input())
-z = int(input())
-print(x+y+z)
-a = int(input('первый катет a = '))
-b = int(input('второй катет b = '))
-print(1/2 * a*b)
-n = int(input('n = '))
-h = n // 60
-if h > 23:
-    h = h % 24
-elif n > 59:
-    n = n % 60
-print(h,' ',n)
-a = int(input('a = '))
-b = int(input('b = '))
-l = int(input('l = '))
-n = int(input('n = '))
-y = a+(a+b)*(n-2)+2*l
+def F(x,y,z)
+return x+y+z
+print(F(1,2,3))
+def F(a,b): #2#
+return 1/2 * a*b
+print(F(7,6))
+def F(n): 
+hours = n % (60 * 24) // 60
+minutes = n % 60
+print(hours, ":", minutes)
+print(F(280000))
+def F(a, b, l, n): 
+return a + (2*n*a) + (2*b*n) + l
+print(F(8,4 , 7, 3))
+def F(x, y, z): #5#
+if x > y and z > y:
 print(y)
-a = int(input())
-b = int(input())
-c = int(input())
-if a < b and a < c:
-    print(a)
-elif b < a and b < c:
-    print(b)
-if c < a and c < b:
-    print(c)
-a = int(input('столбец 1 '))
-b = int(input('строка 1 '))
-c = int(input('столбец 2 '))
-d = int(input('строка 2 '))
+if y > x and z > x:
+print(x)
+if y > z and x > z:
+print(z)
+print(F(3,4,-2))
+def F(a,b,c,d): #6#
 if a % 2 == 0 and b % 2 == 0:
-    if c % 2 == 0 and d % 2 == 0:    
-        print('Да')
+if c % 2 == 0 and d % 2 == 0:
+print('Да')
 if a % 2 == 1 and b % 2 == 1:
-    if c % 2 == 1 and d % 2 == 1:
-        print('Да')
+if c % 2 == 1 and d % 2 == 1:
+print('Да')
 if a % 2 == 0 and b % 2 == 0:
-    if c % 2 == 1 and d % 2 == 1:
-        print('Да')
+if c % 2 == 1 and d % 2 == 1:
+print('Да')
 if a % 2 == 1 and b % 2 == 1:
-    if c % 2 == 0 and d % 2 == 0:
-        print('Да')
-if a % 2 == 0 and b % 2 == 1:
-    if c % 2 == 0 and d % 2 == 0:
-        print('Нет')
+if c % 2 == 0 and d % 2 == 0:
+print('Да')
+if a % 2 == 0 and b % 2 ==1:
+if c % 2 == 0 and d % 2 == 0:
+print('Нет')
+if a % 2 == 1 and b % 2 ==0:
+if c % 2 == 0 and d % 2 == 0:
+print('Нет')
+if a % 2 == 0 and b % 2 ==0:
+if c % 2 == 1 and d % 2 == 0:
+print('Нет')
+if a % 2 == 0 and b % 2 ==0:
+if c % 2 == 0 and d % 2 == 1:
+print('Нет')
+if a % 2 == 1 and b % 2 ==0:
+if c % 2 == 1 and d % 2 == 1:
+print('Нет')
+if a % 2 == 0 and b % 2 ==1:
+if c % 2 == 1 and d % 2 == 1:
+print('Нет')
+if a % 2 == 1 and b % 2 ==1:
+if c % 2 == 0 and d % 2 == 1:
+print('Нет')
+if a % 2 == 1 and b % 2 ==1:
+if c % 2 == 1 and d % 2 == 0:
+print('Нет')
 if a % 2 == 1 and b % 2 == 0:
-    if c % 2 == 0 and d % 2 == 0:
-        print('Нет')
-if a % 2 == 0 and b % 2 == 0:
-    if c % 2 == 1 and d % 2 == 0:
-        print('Нет')
-if a % 2 == 0 and b % 2 == 0:
-    if c % 2 == 0 and d % 2 == 1:
-        print('Нет')
-if a % 2 == 1 and b % 2 == 0:
-    if c % 2 == 1 and d % 2 == 1:
-        print('Нет')
+if c % 2 == 1 and d % 2 == 0:
+print('Да')
 if a % 2 == 0 and b % 2 == 1:
-    if c % 2 == 1 and d % 2 == 1:
-        print('Нет')
-if a % 2 == 1 and b % 2 == 1:
-    if c % 2 == 0 and d % 2 == 1:
-        print('Нет')
-if a % 2 == 1 and b % 2 == 1:
-    if c % 2 == 1 and d % 2 == 0:
-        print('Нет')
+if c % 2 == 0 and d % 2 == 1:
+print('Да')
 if a % 2 == 1 and b % 2 == 0:
-    if c % 2 == 1 and d % 2 == 0:
-        print('Да')
+if c % 2 == 0 and d % 2 == 1:
+print('Да')
 if a % 2 == 0 and b % 2 == 1:
-    if c % 2 == 0 and d % 2 == 1:
-        print('Да')
-if a % 2 == 1 and b % 2 == 0:
-    if c % 2 == 0 and d % 2 == 1:
-        print('Да')
-if a % 2 == 0 and b % 2 == 1:
-    if c % 2 == 1 and d % 2 == 0:
-        print('Да')
-year = int(input())
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print('Да')
+if c % 2 == 1 and d % 2 == 0:
+print('Да')
+print(F(4,3,8,6))
+def F(x): #7#
+if x//4==0 and x//100!=0 and x//400!=0:
+print("Да")
 else:
-    print('Нет')
-x = int(input())
-y = int(input())
-z = int(input())
-if x == y != z or y == z != x or x == z != y:
-    print('2')
-elif x == y == z:
-    print('3')
+print("Нет")
+print(F(2004))
+def F(x, y, z): #8#
+if x == y == z:
+return 3
+if x == y or y==z or z==x:
+return 2
 else:
-    print('0')
-n = int(input())
-m = int(input())
-k = int(input())
-if n * m <= k:
-    print('Нет')
-elif n * m > k:
-    print('Да')
+return 0
+print(F(6, 24, 93))
+def F(n,m,k): #9#
+if m*n//k:
+print("Да")
+else:
+print("Нет")
+print(F(1,5,7))
