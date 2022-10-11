7ce1073f97d6ee31fddc52cd4f4534da4b0b1d5d
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-10-11 19:53:50 +0300

    Update дз3.py

--- дз3.py
+++ дз3.py
@@ -1,93 +1,109 @@
 "# -- coding: utf-8 --"
-def F(x,y,z)
-return x+y+z
-print(F(1,2,3))
-def F(a,b): #2#
-return 1/2 * a*b
-print(F(7,6))
-def F(n): 
-hours = n % (60 * 24) // 60
-minutes = n % 60
-print(hours, ":", minutes)
+задание №1
+def add(x,y,z):                          
+    return x+y+z
+print(add(3,5,8))
+
+задание №2
+def F(a,b):
+    return 1/2 * a*b
+print(F(7,4))
+
+задание №3
+def F(n):
+    hours = n % (60 * 24) // 60
+    minutes = n % 60
+    print(hours, ":", minutes)
 print(F(280000))
-def F(a, b, l, n): 
-return a + (2*n*a) + (2*b*n) + l
+
+задание №4
+def F(a, b, l, n):
+    return a + (2*n*a) + (2*b*n) + l
 print(F(8,4 , 7, 3))
-def F(x, y, z): #5#
-if x > y and z > y:
-print(y)
-if y > x and z > x:
-print(x)
-if y > z and x > z:
-print(z)
-print(F(3,4,-2))
-def F(a,b,c,d): #6#
-if a % 2 == 0 and b % 2 == 0:
-if c % 2 == 0 and d % 2 == 0:
-print('Да')
-if a % 2 == 1 and b % 2 == 1:
-if c % 2 == 1 and d % 2 == 1:
-print('Да')
-if a % 2 == 0 and b % 2 == 0:
-if c % 2 == 1 and d % 2 == 1:
-print('Да')
-if a % 2 == 1 and b % 2 == 1:
-if c % 2 == 0 and d % 2 == 0:
-print('Да')
-if a % 2 == 0 and b % 2 ==1:
-if c % 2 == 0 and d % 2 == 0:
-print('Нет')
-if a % 2 == 1 and b % 2 ==0:
-if c % 2 == 0 and d % 2 == 0:
-print('Нет')
-if a % 2 == 0 and b % 2 ==0:
-if c % 2 == 1 and d % 2 == 0:
-print('Нет')
-if a % 2 == 0 and b % 2 ==0:
-if c % 2 == 0 and d % 2 == 1:
-print('Нет')
-if a % 2 == 1 and b % 2 ==0:
-if c % 2 == 1 and d % 2 == 1:
-print('Нет')
-if a % 2 == 0 and b % 2 ==1:
-if c % 2 == 1 and d % 2 == 1:
-print('Нет')
-if a % 2 == 1 and b % 2 ==1:
-if c % 2 == 0 and d % 2 == 1:
-print('Нет')
-if a % 2 == 1 and b % 2 ==1:
-if c % 2 == 1 and d % 2 == 0:
-print('Нет')
-if a % 2 == 1 and b % 2 == 0:
-if c % 2 == 1 and d % 2 == 0:
-print('Да')
-if a % 2 == 0 and b % 2 == 1:
-if c % 2 == 0 and d % 2 == 1:
-print('Да')
-if a % 2 == 1 and b % 2 == 0:
-if c % 2 == 0 and d % 2 == 1:
-print('Да')
-if a % 2 == 0 and b % 2 == 1:
-if c % 2 == 1 and d % 2 == 0:
-print('Да')
+
+задание№5
+   if x > y and z > y:
+        print(b)
+    if y > x and z > x:
+        print(a)
+    if y > z and x > z:
+        print(z)
+print(F(1,9,-2))
+
+задание №6
+def F(a,b,c,d): 
+    if a % 2 == 0 and b % 2 == 0:
+        if c % 2 == 0 and d % 2 == 0:
+            print('Да')
+    if a % 2 == 1 and b % 2 == 1:
+        if c % 2 == 1 and d % 2 == 1:
+            print('Да')
+    if a % 2 == 0 and b % 2 == 0:
+        if c % 2 == 1 and d % 2 == 1:
+            print('Да')
+    if a % 2 == 1 and b % 2 == 1:
+        if c % 2 == 0 and d % 2 == 0:
+            print('Да')
+    if a % 2 == 0 and b % 2 ==1:
+        if c % 2 == 0 and d % 2 == 0:
+            print('Нет')
+    if a % 2 == 1 and b % 2 ==0:
+        if c % 2 == 0 and d % 2 == 0:
+            print('Нет')
+    if a % 2 == 0 and b % 2 ==0:
+        if c % 2 == 1 and d % 2 == 0:
+            print('Нет')
+    if a % 2 == 0 and b % 2 ==0:
+        if c % 2 == 0 and d % 2 == 1:
+            print('Нет')
+    if a % 2 == 1 and b % 2 ==0:
+        if c % 2 == 1 and d % 2 == 1:
+            print('Нет')
+    if a % 2 == 0 and b % 2 ==1:
+        if c % 2 == 1 and d % 2 == 1:
+            print('Нет')
+    if a % 2 == 1 and b % 2 ==1:
+        if c % 2 == 0 and d % 2 == 1:
+            print('Нет')
+    if a % 2 == 1 and b % 2 ==1:
+        if c % 2 == 1 and d % 2 == 0:
+            print('Нет')
+    if a % 2 == 1 and b % 2 == 0:
+        if c % 2 == 1 and d % 2 == 0:
+            print('Да')
+    if a % 2 == 0 and b % 2 == 1:
+        if c % 2 == 0 and d % 2 == 1:
+            print('Да')
+    if a % 2 == 1 and b % 2 == 0:
+        if c % 2 == 0 and d % 2 == 1:
+            print('Да')
+    if a % 2 == 0 and b % 2 == 1:
+        if c % 2 == 1 and d % 2 == 0:
+            print('Да')
 print(F(4,3,8,6))
-def F(x): #7#
-if x//4==0 and x//100!=0 and x//400!=0:
-print("Да")
-else:
-print("Нет")
+
+задание №7
+def F(x):
+    if x%4==0 and x%100!=0 and x%400!=0:
+        print("Да")
+    else:
+        print("Нет")
 print(F(2004))
-def F(x, y, z): #8#
-if x == y == z:
-return 3
-if x == y or y==z or z==x:
-return 2
-else:
-return 0
-print(F(6, 24, 93))
-def F(n,m,k): #9#
-if m*n//k:
-print("Да")
-else:
-print("Нет")
+
+задание №8
+def F(a, b, c):                              
+    if a == b == c:
+        return 3
+    if a == b or b==c or c==a:
+        return 2
+    else:
+        return 0
+print(F(1, 1, 7))
+
+задание №9
+def F(n,m,k):
+    if m*n//k:
+        print("Да")
+    else:
+        print("Нет")
 print(F(1,5,7))
