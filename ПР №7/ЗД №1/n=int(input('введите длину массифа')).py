6880700eb8f945ae1a8b805280c61f9a3e812cb7
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-11-22 19:44:29 +0300

    Update n=int(input('введите длину массифа')).py

--- ПР №7/ЗД №1/n=int(input('введите длину массифа')).py
+++ ПР №7/ЗД №1/n=int(input('введите длину массифа')).py
@@ -1,10 +1,11 @@
-n=int(input('введите длину массифа'))
-s=[]
-for x in range(n):
-    print('введите элемент')
-    s.append(int(input()))
-print('исходный массиф')
-for x in range(n):
-    if s[x] < 0 and s[x + 1] < 0:
-        print('Пара:', s[x], s[x + 1])
-print()
+def F(n):
+    s = []
+    for x in range(n):
+        print('введите элемент')
+        s.append(int(input()))
+    print('исходный массиф')
+    for x in range(n):
+        if s[x] < 0 and s[x + 1] < 0:
+            print('Пара:', s[x], s[x + 1])
+    print()
+F(10) 
