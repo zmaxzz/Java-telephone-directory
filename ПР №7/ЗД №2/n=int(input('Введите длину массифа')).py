57f372d43fdc1eedecb300bd3bfa88bdfffc625b
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-11-22 19:51:16 +0300

    Update n=int(input('Введите длину массифа')).py

--- ПР №7/ЗД №2/n=int(input('Введите длину массифа')).py
+++ ПР №7/ЗД №2/n=int(input('Введите длину массифа')).py
@@ -1,10 +1,11 @@
-n=int(input('Введите длину массифа'))
-s=[]
-a=[]
-for x in range(n):
-    s.append(int(input()))
-print('исходный массиф')
-for x in s:
-    if x not in a:
-        a.append(x)
-print(a)
+def F(n):
+    s=[]
+    a=[]
+    for x in range(n):
+        s.append(int(input()))
+    print('исходный массиф')
+    for x in s:
+        if x not in a:
+            a.append(x)
+    print(a)
+F(10)
