76b38e524452322d0837a2524277cc344ee01c6c
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-11-29 22:34:26 +0300

    Update sotochka.py

--- ПР №9/ЗД №2/sotochka.py
+++ ПР №9/ЗД №2/sotochka.py
@@ -1,102 +1,98 @@
+from random import randint
 def f():
     n = 3
     m = 3
     ch = 0
     nch = 1
-    a = []
-    for i in range(n):
-        b = []
-        for j in range(3):
-            print('Введите [',i,',',j,'] элемент')
-            b.append(int(input()))
-        a.append(b)
+    a=[randint(0,9) for i in range(3)]
+    b=[randint(0,9) for i in range(3)]
+    c=[randint(0,9) for i in range(3)]
         
-    print(a[0][0],a[0][1],a[0][2])
-    print(a[1][0],a[1][1],a[1][2])
-    print(a[2][0],a[2][1],a[2][2])
+    print(a[0],a[1],a[2])
+    print(b[0],b[1],b[2])
+    print(c[0],c[1],c[2])
     print()
     print()
     #####
-    if a[0][0]>a[0][1]>a[0][2]:
-        min = a[0][2]
+    if a[0]>a[1]>a[2]:
+        min = a[2]
         if min % 2 ==1:
             min = 1
-            print(a[0][0],a[0][1],min)
+            print(a[0],a[1],min)
         else:
             min = 0
-            print(a[0][0],a[0][1],min)
+            print(a[0],a[1],min)
             
                 
-    elif a[0][0]<a[0][1]<a[0][2]:
-        min = a[0][0]
+    elif a[0]<a[1]<a[2]:
+        min = a[0]
         if min % 2 ==1:
             min = 1
-            print(min,a[0][1],a[0][2])
+            print(min,a[1],a[2])
         else:
             min = 0
-            print(min,a[0][1],a[0][2])
+            print(min,a[1],a[2])
     else:
-        min = a[0][1]
+        min = a[1]
         if min % 2 ==1:
             min = 1
-            print(a[0][0],min,a[0][2])
+            print(a[0],min,a[2])
         else:
             min = 0
-            print(a[0][0],min,a[0][2])
+            print(a[0],min,a[2])
 
-    if a[1][0]>a[1][1]>a[1][2]:
-        min = a[1][2]
+    if b[0]>b[1]>b[2]:
+        min = b[2]
         if min % 2 ==1:
             min = 1
-            print(a[1][0],a[1][1],min)
+            print(b[0],b[1],min)
         else:
             min = 0
-            print(a[1][0],a[1][1],min)
+            print(b[0],b[1],min)
             
-    elif a[1][0]<a[1][1]<a[1][2]:
-        min = a[1][0]
+    elif b[0]<b[1]<b[2]:
+        min = b[0]
         if min % 2 ==1:
             min = 1
-            print(min,a[1][1],a[1][2])
+            print(min,b[1],b[2])
         else:
             min = 0
-            print(min,a[1][1],a[1][2])
+            print(min,b[1],b[2])
             
     else:
-        min = a[1][1]
+        min = b[1]
         if min % 2 ==1:
             min = 1
-            print(a[1][0],min,a[1][2])
+            print(b[0],min,b[2])
         else:
             min = 0
-            print(a[1][0],min,a[1][2])
+            print(b[0],min,b[2])
 
-    if a[2][0]>a[2][1]>a[2][2]:
-        min = a[2][2]
+    if c[0]>c[1]>c[2]:
+        min = c[2]
         if min % 2 ==1:
             min = 1
-            print(a[2][0],a[2][1],min)
+            print(c[0],c[1],min)
         else:
             min = 0
-            print(a[2][0],a[2][1],min)
+            print(c[0],c[1],min)
             
-    elif a[2][0]<a[2][1]<a[2][2]:
-        min = a[2][0]
+    elif c[0]<c[1]<c[2]:
+        min = c[0]
         if min % 2 ==1:
             min = 1
-            print(min,a[2][1],a[2][2])
+            print(min,c[1],c[2])
         else:
             min = 0
-            print(min,a[2][1],a[2][2])
+            print(min,c[1],c[2])
             
     else:
-        min = a[2][1]
+        min = c[1]
         if min % 2 ==1:
             min = 1
-            print(a[2][0],min,a[2][2])
+            print(c[0],min,c[2])
         else:
             min = 0
-            print(a[2][0],min,a[2][2])
+            print(c[0],min,c[2])
 f()
-        
 
