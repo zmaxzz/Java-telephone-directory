02ea989703078cfaffb2ede1f3bdd4cfda46734c
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-11-29 20:55:14 +0300

    Update Untitled-1.py

--- ПР №9/ЗД №1/Untitled-1.py
+++ ПР №9/ЗД №1/Untitled-1.py
@@ -1,7 +1,8 @@
+from random import randint
 def f ():
-    stroka1 = [int(input()),int(input()),int(input())]
-    stroka2 = [int(input()),int(input()),int(input())]
-    stroka3 = [int(input()),int(input()),int(input())]
+    stroka1 = [randint(0,5) for i in range(3)]
+    stroka2 = [randint(0,5) for i in range(3)]
+    stroka3 = [randint(0,5) for i in range(3)]
     print(stroka1)
     print(stroka2)
     print(stroka3)
