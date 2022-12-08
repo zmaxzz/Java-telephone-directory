d073eaa304777f907a1fcf16289f3f91a6d58ffc
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-12-08 15:27:09 +0300

    Update sotochka.py

--- ПР №9/ЗД №2/sotochka.py
+++ ПР №9/ЗД №2/sotochka.py
@@ -1,11 +1,13 @@
-from random import randint 
-m=3
-n=3
-a = [[randint(1, 9) for i in range(m)] for j in range(n)]
-for i in a :
-    print(*i)
-print()
-for r in a :
-    r = (list((1 if min(r) % 2 else 0) if j == min(r) else j for j in r))
+def f():
+    from random import randint 
+    m=3
+    n=3
+    a = [[randint(1, 9) for i in range(m)] for j in range(n)]
+    for i in a :
+        print(*i)
+    print()
+    for r in a :
+        r = (list((1 if min(r) % 2 else 0) if j == min(r) else j for j in r))
 
-    print(*r)
+        print(*r)
+f()
