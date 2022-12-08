53db8d2e5f0848cf6d33f98b2a6cd3e84d8ddf29
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-12-08 20:50:40 +0300

    Update import json.py

--- ПР №11/import json.py
+++ ПР №11/import json.py
@@ -1,29 +1,30 @@
+import requests
 import json
-import tkinter as tk
 def F():
     S = name.get()
-    f= """
-    {
-    'company': None
-    'created_at': '2010-11-18T20:33:18Z'
-    'email': None
-    'id': 487568
-    'name': 'Nix/Nixpkgs/NixOS'
-    'url': 'https://api.github.com/users/NixOS'
-    }"""
+    response = requests.get('https://api.github.com/users/NixOS')
 
+    f = json.loads(response.text)
+
+    y = dict.fromkeys(['company'], f['company'])
+    y2 = dict.fromkeys(['created_at'], f['created_at'])
+    y3 = dict.fromkeys(['email'], f['email'])
+    y4 = dict.fromkeys(['id'], f['id'])
+    y5 = dict.fromkeys(['name'], f['name'])
+    y6 = dict.fromkeys(['url'], f['url'])
+    j = (y,y2,y3,y4,y5,y6)
     if S == 'NixOS':
-        with open('C:\\Users\\user\\Desktop\\vivod.json', 'w') as file:
-            json.dump(f,file)      
+        with open('C:\\Users\user\\Desktop\\vivod.json', 'w') as file:
+            json.dump(j,file)      
     else:
         print('Нету')
-
+import tkinter as tk
 window = tk.Tk()
 window.geometry('600x500')
 window.title("interface") 
 name = tk.Entry(window)
-name.grid(padx=160, pady=20)
+name.grid(padx=250, pady=50)
 btn = tk.Button(window, text="      Нажми :)      ", command=F)
-btn.grid(padx=120, pady=20)
+btn.grid(padx=250, pady=50)
 window.mainloop()
 F()
