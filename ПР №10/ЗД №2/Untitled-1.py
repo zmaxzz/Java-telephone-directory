e55b14ec326e8671034cfd342eb8d701d58c187a
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2022-12-09 14:22:31 +0300

    Update Untitled-1.py

--- ПР №10/ЗД №2/Untitled-1.py
+++ ПР №10/ЗД №2/Untitled-1.py
@@ -1,14 +1,7 @@
 F=open(r'C:\\Users\\user\\Desktop\\Bubnov-M.M_Y-224_vvod2.txt')
-L=open(r'C:\\Users\\user\\Desktop\\Bubnov-M.M_Y-224_vvod2.txt')
 S=open(r'C:\\Users\\user\\Desktop\\Bubnov-M.M_Y-224_vivod2.txt','w+')
 Q=F.readlines()
-N=L.readlines(7)
-R=L.readlines(5)
-I=L.readlines(5)
-M=[int(x) for x in N]
-A=[int(x) for x in R]
 P=[int(x) for x in Q]
-T=[int(x) for x in I]
 def f():
     a = [i for i in P]
     S.write(str(a))
