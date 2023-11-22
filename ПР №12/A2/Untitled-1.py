7f3cc20f016d20e61220ec937c611c62ca0ef082
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2023-11-22 12:50:19 +0300

    Delete ПР №12 directory

--- ПР №12/A2/Untitled-1.py
+++
@@ -1,5 +0,0 @@
-def F(a,b):
-    if a%b==0:
-        return 0
-    return a%b
-print(F(43,20))
--- ПР №12/B3/Untitled-1.py
+++
@@ -1,6 +0,0 @@
-def F():
-    x=int(input())
-    if x == 0:
-        return x
-    return max([x,F()])
-print(F())
